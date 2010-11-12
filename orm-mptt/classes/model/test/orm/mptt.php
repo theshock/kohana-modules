eb3862d3650cb8d5546311d6cf67867bd1c7cd37
Shock <shocksilien@gmail.com>
2010-11-12 02:22:23 +0200

    orm-mp

--- orm-mptt/classes/model/test/orm/mptt.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-
-class Model_Test_ORM_MPTT extends ORM_MPTT {
-	protected $_table_name = 'test_orm_mptt';
-}
--- orm-mptt/classes/orm/mptt.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-
-class ORM_MPTT extends Kohana_ORM_MPTT {}
